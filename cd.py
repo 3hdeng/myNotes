0ab6ea3949c2021219307a86a33d0f188eb0e4c8
kelf deng <kelfdeng@gmail.com>
2015-01-08 16:13:20 +0800

    git my documents excluding existent subfolders

---
+++ cd.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+os.chdir('D:/workspaces/ipython')
+#os.system('cd D:/workspaces/ipython')
+#clear the terminal
+#os.system('clear')
+#os.system('D:')
+
+#custom prompt
+#sys.ps1 = os.getcwd()
+#os.environ['PYTHONINSPECT'] = 'True'
+
